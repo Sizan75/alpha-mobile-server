b338fc4b4e6bb5af1293bbd3d636e730685fd837
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-24 23:17:31 +0600

    categories data get done

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express()
 require('dotenv').config();
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const categoryCollection = client.db('alphaMobile').collection('mobileCategories')
+        app.get('/category', async(req,res)=>{
+            const query={}
+            const category=await categoryCollection.find(query).toArray()
+            res.send(category)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.log(err))
 
 app.get('/',(req,res)=>{
     res.send('doctors portal server is running')
